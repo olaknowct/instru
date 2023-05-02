21d3309c110f2bbe09d20c74dca2adf879883909
Christopher Olano <olanochristopher@gmail.com>
2023-05-03 01:39:44 +0800

    add error handling add signup  controller

--- serverless-instru/controllers/userController.js
+++ serverless-instru/controllers/userController.js
@@ -1,23 +1,41 @@
 const User = require('../models/userModel');
 
 exports.getUser = async (req, res) => {
-  const userId = req.params.userId;
   try {
-    const response = await User.getUser(userId);
+    const userId = req.params.userId;
+    const response = await User.get(userId);
 
     res.status(200).json({ users: response });
   } catch (error) {
-    console.log(error);
+    console.log('error 💥 ', error);
+    res.status(404).json({ error: error.message });
   }
 };
 
 exports.getUsers = async (req, res) => {
   try {
-    const response = await User.getUsers();
-
+    const response = await User.getAll();
     res.status(200).json({ users: response });
   } catch (error) {
-    console.log(error);
+    console.log('error 💥 ', error);
+    res.status(404).json({ error: error.message });
+  }
+};
+
+exports.signUp = async (req, res, next) => {
+  try {
+    const { name, email, password, passwordConfirm } = req.body;
+    const newUser = await User.create({
+      name,
+      email,
+      password,
+      passwordConfirm,
+    });
+
+    res.status(200).json({ user: newUser });
+  } catch (error) {
+    console.log('error 💥 ', error);
+    res.status(404).json({ error: error.message });
   }
 };
 
