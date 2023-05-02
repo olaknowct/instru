2a4b98871fea8728e9bf45865a76a48d0aebd5d9
Christopher Olano <olanochristopher@gmail.com>
2023-05-02 21:17:18 +0800

    update handler

---
+++ serverless-instru/handler.js
@@ -0,0 +1,14 @@
+const serverless = require('serverless-http');
+const express = require('express');
+const app = express();
+
+const userRoutes = require('./routes/userRoutes');
+
+app.use(express.json());
+
+app.use('/users', userRoutes);
+
+module.exports = {
+  getUser: serverless(app),
+  getUsers: serverless(app),
+};
--- serverless-instru/index.js
+++
@@ -1,9 +0,0 @@
-const serverless = require('serverless-http');
-const express = require('express');
-const app = express();
-const usersRouter = require('./routes/userRoutes');
-
-app.use(express.json());
-app.use('/users', usersRouter);
-
-module.exports.handler = serverless(app);
