be0a4ec60c85ab2860a573603f13b3eb96536655
Christopher Olano <olanochristopher@gmail.com>
2023-05-03 01:40:41 +0800

    add post signup routes

--- serverless-instru/routes/userRoutes.js
+++ serverless-instru/routes/userRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { getUsers, getUser } = require('../controllers/userController');
+const { getUsers, getUser, signUp } = require('../controllers/userController');
 
 const router = express.Router();
 
 router.get('/', getUsers);
 router.get('/:userId', getUser);
+router.post('/signup', signUp);
 
 module.exports = router;
