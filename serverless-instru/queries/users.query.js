a6a1ad1bda47e2045bd9567c4fc2f359b7d0d160
Christopher Olano <olanochristopher@gmail.com>
2023-05-04 22:50:34 +0800

    add count email query

--- serverless-instru/queries/users.query.js
+++ serverless-instru/queries/users.query.js
@@ -1,9 +1,11 @@
 const selectUserByIdQuery = 'SELECT * FROM users WHERE id = ?';
 const selectAllUsersQuery = 'SELECT * FROM users';
 const insertUserQuery = 'INSERT INTO users (name, email, password) VALUES (?, ?, ?)';
+const countEmailQuery = 'SELECT COUNT(*) as count FROM users WHERE email = ?';
 
 module.exports = {
   selectUserByIdQuery,
   selectAllUsersQuery,
   insertUserQuery,
+  countEmailQuery,
 };
